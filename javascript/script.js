c2d5f99d546292534f3e378e9c715274843aa9ba
Gracie Lundell <gracielundell@gmail.com>
2015-11-07 15:41:12 -0800

    added .js file, created program's function code (for loop: ping, pong, pingpong

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+var ul = document.createElement("ul");
+
+for(var i = 1; i <= 20; i++) {
+  var li = document.createElement("li");
+
+    if (i % 3 === 0 && i % 5 ===0) {
+      li.innerHTML = ("pingpong");
+    }
+    else if (i % 3 === 0) {
+      li.innerHTML = ("ping");
+    }
+    else if (i % 5 === 0) {
+      li.innerHTML = ("pong");
+    }
+    else {
+      li.innerHTML = (i);
+    }
+  ul.appendChild(li)
+}
+
+document.getElementById("pingpongcode").appendChild(ul);
