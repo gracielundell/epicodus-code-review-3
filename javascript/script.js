0d5acb6e9dff3951f579b48ac308127603d343e6
Gracie Lundell <gracielundell@gmail.com>
2015-11-11 20:47:36 -0800

    Make page not refresh by default, make it so that when you press the submit button the output doesn't repeat

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,34 @@
-var ul = document.createElement("ul");
-
-for(var i = 1; i <= 20; i++) {
-  var li = document.createElement("li");
-
-    if (i % 3 === 0 && i % 5 ===0) {
-      li.innerHTML = ("pingpong");
-    }
-    else if (i % 3 === 0) {
-      li.innerHTML = ("ping");
-    }
-    else if (i % 5 === 0) {
-      li.innerHTML = ("pong");
-    }
-    else {
-      li.innerHTML = (i);
-    }
-  ul.appendChild(li)
+function reg() {
+  window.captureEvents(Event.SUBMIT);
+  window.onsubmit = hit;
 }
 
-document.getElementById("pingpongcode").appendChild(ul);
+function hit(evt) {
+  evt.preventDefault();
+
+  document.getElementById("pingpongcode").innerHTML = ""
+
+  var ul = document.createElement("ul");
+
+  // var userNumber = (userinput) //from BS input box
+
+  for(var i = 1; i <= 20; i++) {
+    var li = document.createElement("li");
+
+      if (i % 3 === 0 && i % 5 ===0) {
+        li.innerHTML = "pingpong";
+      }
+      else if (i % 3 === 0) {
+        li.innerHTML = "ping";
+      }
+      else if (i % 5 === 0) {
+        li.innerHTML = "pong";
+      }
+      else {
+        li.innerHTML = i;
+      }
+    ul.appendChild(li)
+  }
+
+  document.getElementById("pingpongcode").appendChild(ul);
+}
